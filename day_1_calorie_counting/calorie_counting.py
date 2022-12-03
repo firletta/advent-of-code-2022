854c1cc116656a636c454163d8cfb0a0d1e8525a
Anna Alduz <firletta@gmail.com>
2022-12-04 00:01:05 +0100

    elfs, part two solved

--- day_1_calorie_counting/calorie_counting.py
+++ day_1_calorie_counting/calorie_counting.py
@@ -1,13 +1,6 @@
-import os.path
-
-try:
-    file = open("input.txt","r")
-except IOError:
-    print("Error:\n'input.txt' was not found.")
-    exit()
-
 input_file = open("input.txt","r")
 input_data = input_file.read()
+
 input_lists = input_data.split("\n\n")
 elves_calories_listed = []
 elves_calories_sum = []
@@ -20,7 +13,15 @@ for i in input_lists:
     elves_calories_sum.append(calories_sum)
     elves_calories_listed.append(split)
 
-winner = max(elves_calories_sum)
+first_place = max(elves_calories_sum)
 
+# print(f"Elf number {elves_calories_sum.index(first_place) + 1} has the most calories, which is: {first_place}")
 
-print(f"Elf number {elves_calories_sum.index(winner) + 1} has the most calories, which is: {winner}")
+elves_calories_sum.remove(first_place)
+second_place = max(elves_calories_sum)
+elves_calories_sum.remove(second_place)
+third_place = max(elves_calories_sum)
+
+top_three = first_place + second_place + third_place
+print(first_place, second_place, third_place)
+print(top_three)
